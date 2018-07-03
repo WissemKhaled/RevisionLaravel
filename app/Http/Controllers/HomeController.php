d08a91495ab9395f9736d74be5c293212c714cea
Wissem <wis@wis.wiss>
2018-07-03 14:22:09 +0000

    Good Strating Laravel

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+       // $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function accueil()
+    {
+        if (Auth::user()) {
+            dd('t\'es in',  Auth::user()->name);
+        } else {
+            dd('t\'es out');
+        }
+    }
+}
